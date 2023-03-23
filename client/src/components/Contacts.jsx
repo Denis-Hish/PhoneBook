891ccb1b56295cc7ebc93d512916e144a2a3025d
Denis-Hish <denis.hish@gmail.com>
2023-03-23 12:56:33 +0100

    Update Contacts.jsx

--- client/src/components/Contacts.jsx
+++ client/src/components/Contacts.jsx
@@ -1,41 +1,52 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
 import { getAllContacts } from '../services/paramsAPI';
 
 const Contacts = () => {
-  const [contacts, setContacts] = useState(null);
+   const [contacts, setContacts] = useState(null);
 
-  const getContacts = async () => {
-    let res = await getAllContacts();
+   const getContacts = async () => {
+      let res = await getAllContacts();
 
-    if (res instanceof Array && !res.length) {
-      console.log("---No Contacts in DB -", res.length);
-    };
-    
-    setContacts(res);
-  }
+      if (res instanceof Array && !res.length) {
+         console.log('---No Contacts in DB -', res.length);
+      }
 
-  useEffect(() => {
-    if(!contacts) {
-      getContacts();
-    }
-  })
+      setContacts(res);
+   };
 
-  console.log("---All contacts---", contacts);
+   useEffect(() => {
+      if (!contacts) {
+         getContacts();
+      }
+   });
 
-  return (
-    <div>
-      <h2>Contacts:</h2>
-      <table>
-        {contacts?.map(({id, userName, phoneNumber}) =>
-          <tr key={id}>
-            <td>{userName}:</td>
-            <td>{phoneNumber}</td>
-          </tr>
-          )
-        }
-      </table>  
-    </div>
-  );
+   console.log('---All contacts---', contacts);
+
+   return (
+      <div>
+         <h2>Contacts:</h2>
+         <table>
+            {contacts?.map(
+               ({
+                  id,
+                  userName,
+                  phoneNumber1,
+                  phoneNumber2,
+                  phoneNumber3,
+                  group,
+               }) => (
+                  <tr key={id}>
+                     <td>{userName}:</td>
+                     <td>{phoneNumber1}</td>
+                     <td>{phoneNumber2}</td>
+                     <td>{phoneNumber3}</td>
+                     <td>{group}</td>
+                  </tr>
+               )
+            )}
+         </table>
+      </div>
+   );
 };
 
 export default Contacts;
