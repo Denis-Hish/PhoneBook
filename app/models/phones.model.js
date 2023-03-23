9f05bf955d1aeacd4b2373c8dd7219e300ba36a0
Denis-Hish <denis.hish@gmail.com>
2023-03-23 12:06:21 +0100

    Add phoneNumber2: String, phoneNumber3: String,  group: String,

--- app/models/phones.model.js
+++ app/models/phones.model.js
@@ -1,20 +1,23 @@
 const mongoose = require('mongoose');
 
 module.exports = mongoose => {
-  const schema = mongoose.Schema(
-    {
-      userName: String,
-      phoneNumber: String
-    },
-    { timestamps: true }
-  );
+   const schema = mongoose.Schema(
+      {
+         userName: String,
+         phoneNumber1: String,
+         phoneNumber2: String,
+         phoneNumber3: String,
+         group: String,
+      },
+      { timestamps: true }
+   );
 
-  schema.method("toJSON", function() {
-    const { __v, _id, ...object } = this.toObject();
-    object.id = _id;
-    return object;
-  });
+   schema.method('toJSON', function () {
+      const { __v, _id, ...object } = this.toObject();
+      object.id = _id;
+      return object;
+   });
 
-  const ContactDetails = mongoose.model("phones", schema);
-  return ContactDetails;
+   const ContactDetails = mongoose.model('phones', schema);
+   return ContactDetails;
 };
