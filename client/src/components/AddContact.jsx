d1059ddbcc8df40ad3f2e98f1f1b2eb254e2d457
Denis-Hish <denis.hish@gmail.com>
2023-03-23 12:16:57 +0100

    Add inputs

--- client/src/components/AddContact.jsx
+++ client/src/components/AddContact.jsx
@@ -2,41 +2,93 @@ import { useState } from 'react';
 import { addContact } from '../services/paramsAPI';
 
 const AddContacts = () => {
-  const [contact, setContact] = useState()
+   const [contact, setContact] = useState();
 
-  const onChangeHandler = (event) => {
-    const {name, value} = event
-      setContact((prev) => {
-        return {...prev, [name]: value}
-      })
-  }
+   const onChangeHandler = event => {
+      const { name, value } = event;
+      setContact(prev => {
+         return { ...prev, [name]: value };
+      });
+   };
 
-  const sumbitForm = (event) => { // TODO: Add validation
-    event.preventDefault()
-    console.log("--contact--", contact)
-    addContact(contact);
-  }
+   const sumbitForm = event => {
+      // TODO: Add validation
+      event.preventDefault();
+      console.log('--contact--', contact);
+      addContact(contact);
+   };
 
-  return (
-    <div>
-      <h2>Add Contact:</h2>
-      <form onSubmit={sumbitForm}>
-        <table>
-          <tbody>
-            <tr><td>Name:</td><td>
-              <input type="text" name="userName" value={contact?.userName} onChange={(e) => onChangeHandler(e.target)} />
-              </td></tr>
-            <tr><td>Phone:</td><td>
-              <input type="text" name="phoneNumber" value={contact?.phoneNumber} onChange={(e) => onChangeHandler(e.target)} />
-            </td></tr>
-            <tr><td>
-              <button type="submit">Submit</button>
-            </td></tr>
-          </tbody>
-        </table>
-      </form>
-    </div>
-  );
+   return (
+      <div>
+         <h2>Add Contact:</h2>
+         <form onSubmit={sumbitForm}>
+            <table>
+               <tbody>
+                  <tr>
+                     <td>Name:</td>
+                     <td>
+                        <input
+                           type="text"
+                           name="userName"
+                           value={contact?.userName}
+                           onChange={e => onChangeHandler(e.target)}
+                        />
+                     </td>
+                  </tr>
+                  <tr>
+                     <td>Phone1:</td>
+                     <td>
+                        <input
+                           type="text"
+                           name="phoneNumber1"
+                           value={contact?.phoneNumber}
+                           onChange={e => onChangeHandler(e.target)}
+                        />
+                     </td>
+                  </tr>
+                  <tr>
+                     <td>Phone2:</td>
+                     <td>
+                        <input
+                           type="text"
+                           name="phoneNumber2"
+                           value={contact?.phoneNumber}
+                           onChange={e => onChangeHandler(e.target)}
+                        />
+                     </td>
+                  </tr>
+                  <tr>
+                     <td>Phone3:</td>
+                     <td>
+                        <input
+                           type="text"
+                           name="phoneNumber3"
+                           value={contact?.phoneNumber}
+                           onChange={e => onChangeHandler(e.target)}
+                        />
+                     </td>
+                  </tr>
+                  <tr>
+                     <td>Group:</td>
+                     <td>
+                        <input
+                           type="text"
+                           name="group"
+                           value={contact?.phoneNumber}
+                           onChange={e => onChangeHandler(e.target)}
+                        />
+                     </td>
+                  </tr>
+                  <tr>
+                     <td>
+                        <button type="submit">Submit</button>
+                     </td>
+                  </tr>
+               </tbody>
+            </table>
+         </form>
+      </div>
+   );
 };
 
 export default AddContacts;
