f551231a5c297881f3fc78598f57f77c21f652f6
Denis-Hish <denis.hish@gmail.com>
2023-06-28 11:18:53 +0200

    Add convertert to XML

--- client/src/components/Converter.jsx
+++ client/src/components/Converter.jsx
@@ -1,21 +1,75 @@
 import React from 'react';
 import Button from '@mui/material/Button';
 import { getAllContacts } from '../services/paramsAPI';
+import { create } from 'xmlbuilder2';
 
 const Converter = () => {
-   const allContacts = async () => {
+   const handleConvert = async () => {
       const contacts = await getAllContacts();
-      // console.log('Converter.jxs - All contacts:', contacts);
 
-      // contacts.forEach((contact) => {
-      //    console.log(contact.userName);
-      // });
+      const xml1 = create({ version: '1.0', encoding: 'UTF-8' })
+         .ele('YealinkIPPhoneBook')
+         .ele('Title')
+         .txt('Yealink')
+         .up();
+
+      const xml2 = create({ version: '1.0', encoding: 'UTF-8' })
+         .ele('root_group')
+         .ele('group', { display_name: 'All Contacts', ring: 'Auto' })
+         .up();
+
+      contacts.forEach((contact) => {
+         const { userName, phoneNumber1, phoneNumber2, phoneNumber3, group } = contact;
+
+         xml1
+            .ele('Menu', { Name: group })
+            .ele('Unit', {
+               Name: userName,
+               Phone1: phoneNumber1,
+               Phone2: phoneNumber2,
+               Phone3: phoneNumber3,
+               default_photo: 'Resource:',
+            })
+            .up()
+            .up();
+
+         xml2
+            .ele('group', { display_name: group, ring: 'Auto' })
+            .up()
+            .ele('contact', {
+               display_name: userName,
+               office_number: phoneNumber1,
+               mobile_number: phoneNumber2,
+               other_number: phoneNumber3,
+               line: '0',
+               ring: 'Auto',
+               group_id_name: group,
+            })
+            .up();
+      });
+
+      const xmlString1 = xml1.end({ prettyPrint: true });
+      const xmlString2 = xml2.end({ prettyPrint: true });
+
+      // Сохраняем xmlString1 в файл file1.xml
+      const file1Blob = new Blob([xmlString1], { type: 'text/xml' });
+      const file1Url = URL.createObjectURL(file1Blob);
+      const file1Link = document.createElement('a');
+      file1Link.href = file1Url;
+      file1Link.download = 'TEST_file1.xml';
+      file1Link.click();
+
+      // Сохраняем xmlString2 в файл file2.xml
+      const file2Blob = new Blob([xmlString2], { type: 'text/xml' });
+      const file2Url = URL.createObjectURL(file2Blob);
+      const file2Link = document.createElement('a');
+      file2Link.href = file2Url;
+      file2Link.download = 'TEST_file2.xml';
+      file2Link.click();
    };
 
-   allContacts();
-
    return (
-      <Button variant="outlined" color="error">
+      <Button variant="outlined" color="error" onClick={handleConvert}>
          Convert to xml
       </Button>
    );
