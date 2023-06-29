30a0600fb96d52bf42d176d041919942e038810b
Denis-Hish <denis.hish@gmail.com>
2023-06-29 10:59:22 +0200

    Added button clear input filter and change her style css

--- client/src/components/Filter.jsx
+++ client/src/components/Filter.jsx
@@ -5,6 +5,10 @@ import IconButton from '@mui/material/IconButton';
 import ClearIcon from '@mui/icons-material/Clear';
 
 export default function Filter({ value, onChange }) {
+   const handleClear = () => {
+      onChange({ target: { value: '' } }); // Обновляем значение формы на пустую строку
+   };
+
    return (
       <Box className="filter" component="form" noValidate autoComplete="off">
          <TextField
@@ -16,7 +20,7 @@ export default function Filter({ value, onChange }) {
             onChange={onChange}
          />
 
-         <IconButton className="clear-filter-btn">
+         <IconButton className="clear-filter-btn" onClick={handleClear}>
             <ClearIcon />
          </IconButton>
       </Box>
