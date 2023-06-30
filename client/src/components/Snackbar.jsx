0232dacb0886cde6fb3da2be1f7b953f3b28601d
Denis-Hish <denis.hish@gmail.com>
2023-06-30 11:22:51 +0200

    Added transition for Snackbar

--- client/src/components/Snackbar.jsx
+++ client/src/components/Snackbar.jsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import Snackbar from '@mui/material/Snackbar';
+import Slide from '@mui/material/Slide';
 import MuiAlert from '@mui/material/Alert';
 
 const Alert = React.forwardRef(function Alert(props, ref) {
@@ -8,10 +9,12 @@ const Alert = React.forwardRef(function Alert(props, ref) {
 
 export default function CustomizedSnackbars({ message, color }) {
    const [open, setOpen] = React.useState(false);
+   const [transition, setTransition] = React.useState(undefined);
 
    React.useEffect(() => {
       if (message) {
          setOpen(true); // Open the Snackbar when a message is set
+         setTransition(() => Transition);
       }
    }, [message]);
 
@@ -19,12 +22,17 @@ export default function CustomizedSnackbars({ message, color }) {
       setOpen(false);
    };
 
+   function Transition(props) {
+      return <Slide {...props} direction="right" />;
+   }
+
    return (
       <Snackbar
          open={open}
-         autoHideDuration={5000}
+         autoHideDuration={6000}
          onClose={handleClose}
          anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }}
+         TransitionComponent={transition}
       >
          <Alert className="snackbar" onClose={handleClose} severity={color} sx={{ width: '100%' }}>
             {message}
