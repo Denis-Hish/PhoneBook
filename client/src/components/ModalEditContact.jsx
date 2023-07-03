23fab101974a13a8937d8d73d0ec9db49c507bc6
Denis-Hish <denis.hish@gmail.com>
2023-07-03 13:00:42 +0200

    Added contact editing function!

--- client/src/components/ModalEditContact.jsx
+++ client/src/components/ModalEditContact.jsx
@@ -1,11 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Backdrop from '@mui/material/Backdrop';
 import Box from '@mui/material/Box';
 import Modal from '@mui/material/Modal';
 import Typography from '@mui/material/Typography';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
-import { addContact } from '../services/paramsAPI';
 import { editContact } from '../services/paramsAPI';
 
 const style = {
@@ -26,6 +25,20 @@ const ModalEditContact = ({ contact, openModal, setOpenModal }) => {
    const closeModal = () => {
       setOpenModal(false);
    };
+
+   const [name, setName] = useState(userName || '');
+   const [phone1, setPhone1] = useState(phoneNumber1 || '');
+   const [phone2, setPhone2] = useState(phoneNumber2 || '');
+   const [phone3, setPhone3] = useState(phoneNumber3 || '');
+   const [group1, setGroup] = useState(group || '');
+
+   useEffect(() => {
+      setName(userName || '');
+      setPhone1(phoneNumber1 || '');
+      setPhone2(phoneNumber2 || '');
+      setPhone3(phoneNumber3 || '');
+      setGroup(group || '');
+   }, [contact]);
 
    return (
       <div>
@@ -48,8 +61,16 @@ const ModalEditContact = ({ contact, openModal, setOpenModal }) => {
                   component="form"
                   onSubmit={(e) => {
                      e.preventDefault();
-                     console.log('Редактируемый контакт', contact);
-                     editContact({ id, userName, phoneNumber1, phoneNumber2, phoneNumber3, group });
+                     const updatedContact = {
+                        id,
+                        userName: name,
+                        phoneNumber1: phone1,
+                        phoneNumber2: phone2,
+                        phoneNumber3: phone3,
+                        group: group1,
+                     };
+                     console.log('Редактируемый контакт', updatedContact);
+                     editContact(id, updatedContact);
                   }}
                >
                   <Typography id="transition-modal-title" variant="h6" component="h2">
@@ -57,16 +78,45 @@ const ModalEditContact = ({ contact, openModal, setOpenModal }) => {
                   </Typography>
 
                   <TextField
-                     id="outlined-basic"
+                     // id="outlined-basic"
                      label="Name"
                      variant="outlined"
                      // required // обязательное для заполнения
-                     defaultValue={userName}
+                     value={name}
+                     onChange={(e) => setName(e.target.value)}
                   />
-                  <TextField id="outlined-basic" label="Phone 1" variant="outlined" defaultValue={phoneNumber1} />
-                  <TextField id="outlined-basic" label="Phone 2" variant="outlined" defaultValue={phoneNumber2} />
-                  <TextField id="outlined-basic" label="Phone 3" variant="outlined" defaultValue={phoneNumber3} />
-                  <TextField id="outlined-basic" label="Group" variant="outlined" defaultValue={group} />
+                  <TextField
+                     // id="outlined-basic"
+                     label="Phone 1"
+                     variant="outlined"
+                     // required // обязательное для заполнения
+                     value={phone1}
+                     onChange={(e) => setPhone1(e.target.value)}
+                  />
+                  <TextField
+                     // id="outlined-basic"
+                     label="Phone 2"
+                     variant="outlined"
+                     // required // обязательное для заполнения
+                     value={phone2}
+                     onChange={(e) => setPhone2(e.target.value)}
+                  />
+                  <TextField
+                     // id="outlined-basic"
+                     label="Phone 3"
+                     variant="outlined"
+                     // required // обязательное для заполнения
+                     value={phone3}
+                     onChange={(e) => setPhone3(e.target.value)}
+                  />
+                  <TextField
+                     // id="outlined-basic"
+                     label="Group"
+                     variant="outlined"
+                     // required // обязательное для заполнения
+                     value={group1}
+                     onChange={(e) => setGroup(e.target.value)}
+                  />
                   <Button type="submit" variant="outlined">
                      Save contact
                   </Button>
