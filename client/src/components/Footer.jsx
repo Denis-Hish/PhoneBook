2e7935cf619cd5d2fe71e6f59c92723db48b95b2
Denis-Hish <denis.hish@gmail.com>
2023-08-22 10:07:54 +0200

    Small changes CSS styles

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 const Footer = () => {
    return (
       <footer className="footer">
-         <div className="container">
-            <p>Denis Nazarow &copy; 2023</p>
-         </div>
+         {/* <div className="container"> */}
+         <p>Denis Nazarow &copy; 2023</p>
+         {/* </div> */}
       </footer>
    );
 };
