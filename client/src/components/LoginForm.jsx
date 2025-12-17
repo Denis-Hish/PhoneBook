e0c261246e4e2022fde560e8f37f2a8ce93b743a
Denis-Hish <denis.hish@gmail.com>
2025-12-17 10:15:40 +0100

    Small security update

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -9,162 +9,184 @@ import { setMessage } from '../components/Snackbar';
 import axios from 'axios';
 
 const LoginForm = ({ onLogin }) => {
-   const rememberedUsername = localStorage.getItem('rememberedUsername');
-   const initialUsername = rememberedUsername ? rememberedUsername : '';
-   const [username, setUsername] = useState(initialUsername);
+  const rememberedUsername = localStorage.getItem('rememberedUsername');
+  const initialUsername = rememberedUsername ? rememberedUsername : 'admin';
+  const [username, setUsername] = useState(initialUsername);
 
-   const [password, setPassword] = useState('');
-   const [isUsernameFocused, setIsUsernameFocused] = useState(false);
-   const [isPasswordFocused, setIsPasswordFocused] = useState(false);
-   const [rememberMe, setRememberMe] = useState(false);
-   const { t } = useTranslation();
+  const [password, setPassword] = useState('admin');
+  const [isUsernameFocused, setIsUsernameFocused] = useState(false);
+  const [isPasswordFocused, setIsPasswordFocused] = useState(false);
+  const [rememberMe, setRememberMe] = useState(false);
+  const { t } = useTranslation();
 
-   // Local authentication
-   // const handleSubmit = (e) => {
-   //    e.preventDefault();
-   //    // Проверка и установка логина и пароля
-   //    if (username === 'admin' && password === '') {
-   //       onLogin();
-   //       setMessage({ message: t('welcome'), color: 'success' });
-   //       if (rememberMe) {
-   //          // Сохраняем имя пользователя в localStorage
-   //          localStorage.setItem('rememberedUsername', username);
-   //       } else {
-   //          // Если галочка "Remember Me" снята, удаляем сохраненное имя пользователя из localStorage
-   //          localStorage.removeItem('rememberedUsername');
-   //       }
-   //    } else {
-   //       const message = t('wrong_login');
-   //       const color = 'error';
-   //       setMessage({ message, color });
-   //    }
-   // };
+  // Local authentication
+  // const handleSubmit = (e) => {
+  //    e.preventDefault();
+  //    // Проверка и установка логина и пароля
+  //    if (username === 'admin' && password === '') {
+  //       onLogin();
+  //       setMessage({ message: t('welcome'), color: 'success' });
+  //       if (rememberMe) {
+  //          // Сохраняем имя пользователя в localStorage
+  //          localStorage.setItem('rememberedUsername', username);
+  //       } else {
+  //          // Если галочка "Remember Me" снята, удаляем сохраненное имя пользователя из localStorage
+  //          localStorage.removeItem('rememberedUsername');
+  //       }
+  //    } else {
+  //       const message = t('wrong_login');
+  //       const color = 'error';
+  //       setMessage({ message, color });
+  //    }
+  // };
 
-   // MongoDB authentication
-   const handleSubmit = async (e) => {
-      e.preventDefault();
+  // MongoDB authentication
+  const handleSubmit = async e => {
+    e.preventDefault();
 
-      try {
-         const response = await axios.post('/api/auth/login', { username, password });
+    try {
+      const response = await axios.post('/api/auth/login', {
+        username,
+        password,
+      });
 
-         if (response.data.success) {
-            // Успешная аутентификация
-            onLogin();
-            setMessage({ message: t('welcome'), color: 'success' });
-            if (rememberMe) {
-               localStorage.setItem('rememberedUsername', username);
-            } else {
-               localStorage.removeItem('rememberedUsername');
-            }
-         } else {
-            // Ошибка аутентификации
-            const errorCode = response.data.errorCode; // Здесь подставьте правильное поле с идентификатором ошибки
-            const message = errorCode === 'invalid_credentials' ? t('wrong_login') : t('other_error_message');
-            const color = 'error';
-            setMessage({ message, color });
-         }
-      } catch (error) {
-         // Обработка ошибки
-         if (error.response && error.response.status === 401) {
-            const message = t('wrong_login');
-            const color = 'error';
-            setMessage({ message, color });
-         } else {
-            // Ошибка при запросе
-            console.error('Error during login:', error);
-         }
-      }
-   };
-
-   // Запомнить имя пользователя
-   const handleRememberMeChange = (e) => {
-      setRememberMe(e.target.checked);
-      if (e.target.checked) {
-         // Если галочка "Remember Me" установлена, получаем имя пользователя из localStorage (если есть)
-         const rememberedUsername = localStorage.getItem('rememberedUsername');
-         if (rememberedUsername) {
-            setUsername(rememberedUsername);
-         }
+      if (response.data.success) {
+        // Успешная аутентификация
+        onLogin();
+        setMessage({ message: t('welcome'), color: 'success' });
+        if (rememberMe) {
+          localStorage.setItem('rememberedUsername', username);
+        } else {
+          localStorage.removeItem('rememberedUsername');
+        }
       } else {
-         // Если галочка "Remember Me" снята, удаляем сохраненное имя пользователя из localStorage
-         localStorage.removeItem('rememberedUsername');
+        // Ошибка аутентификации
+        const errorCode = response.data.errorCode; // Здесь подставьте правильное поле с идентификатором ошибки
+        const message =
+          errorCode === 'invalid_credentials'
+            ? t('wrong_login')
+            : t('other_error_message');
+        const color = 'error';
+        setMessage({ message, color });
       }
-   };
+    } catch (error) {
+      // Обработка ошибки
+      if (error.response && error.response.status === 401) {
+        const message = t('wrong_login');
+        const color = 'error';
+        setMessage({ message, color });
+      } else {
+        // Ошибка при запросе
+        console.error('Error during login:', error);
+      }
+    }
+  };
 
-   // Проверка наличия данных в localStorage и установка состояния галочки "Remember Me"
-   useEffect(() => {
+  // Запомнить имя пользователя
+  const handleRememberMeChange = e => {
+    setRememberMe(e.target.checked);
+    if (e.target.checked) {
+      // Если галочка "Remember Me" установлена, получаем имя пользователя из localStorage (если есть)
       const rememberedUsername = localStorage.getItem('rememberedUsername');
-      setRememberMe(!!rememberedUsername);
-   }, []);
+      if (rememberedUsername) {
+        setUsername(rememberedUsername);
+      }
+    } else {
+      // Если галочка "Remember Me" снята, удаляем сохраненное имя пользователя из localStorage
+      localStorage.removeItem('rememberedUsername');
+    }
+  };
 
-   // Clear input
-   const handleClearUsername = () => {
-      setUsername('');
-   };
+  // Проверка наличия данных в localStorage и установка состояния галочки "Remember Me"
+  useEffect(() => {
+    const rememberedUsername = localStorage.getItem('rememberedUsername');
+    setRememberMe(!!rememberedUsername);
+  }, []);
 
-   const handleClearPassword = () => {
-      setPassword('');
-   };
+  // Clear input
+  const handleClearUsername = () => {
+    setUsername('');
+  };
 
-   return (
-      <div className="login-wrapper">
-         <div className="login">
-            <form onSubmit={handleSubmit}>
-               <h2>
-                  {t('hello')}
-                  <br />
-                  <span>{t('welcome_back')}</span>
-               </h2>
-               <div className="input-box">
-                  <TextField
-                     className="input-form"
-                     type="text"
-                     name="username"
-                     label={t('user_name')}
-                     value={username}
-                     onChange={(e) => setUsername(e.target.value)}
-                     variant="standard"
-                     autoComplete="off"
-                     onMouseEnter={() => setIsUsernameFocused(true)}
-                  />
-                  {isUsernameFocused && username !== '' && (
-                     <IconButton className="clear-btn" onClick={handleClearUsername} tabIndex={-1}>
-                        <ClearIcon />
-                     </IconButton>
-                  )}
-                  <PersonIcon className="icons" />
-               </div>
-               <div className="input-box">
-                  <TextField
-                     className="input-form"
-                     type="password"
-                     name="password"
-                     label={t('password')}
-                     value={password}
-                     onChange={(e) => setPassword(e.target.value)}
-                     variant="standard"
-                     onMouseEnter={() => setIsPasswordFocused(true)}
-                  />
-                  {isPasswordFocused && password !== '' && (
-                     <IconButton className="clear-btn" onClick={handleClearPassword} tabIndex={-1}>
-                        <ClearIcon />
-                     </IconButton>
-                  )}
-                  <LockIcon className="icons" />
-               </div>
-               <label className="remember-me-form">
-                  <input type="checkbox" checked={rememberMe} onChange={handleRememberMeChange} />
-                  {t('remember_me')}
-               </label>
-               <div className="input-box">
-                  <Button className="btn-login btn-blue" type="submit" variant="outlined">
-                     {t('login')}
-                  </Button>
-               </div>
-            </form>
-         </div>
+  const handleClearPassword = () => {
+    setPassword('');
+  };
+
+  return (
+    <div className='login-wrapper'>
+      <div className='login'>
+        <form onSubmit={handleSubmit}>
+          <h2>
+            {t('hello')}
+            <br />
+            <span>{t('welcome_back')}</span>
+          </h2>
+          <div className='input-box'>
+            <TextField
+              className='input-form'
+              type='text'
+              name='username'
+              label={t('user_name')}
+              value={username}
+              onChange={e => setUsername(e.target.value)}
+              variant='standard'
+              autoComplete='off'
+              onMouseEnter={() => setIsUsernameFocused(true)}
+            />
+            {isUsernameFocused && username !== '' && (
+              <IconButton
+                className='clear-btn'
+                onClick={handleClearUsername}
+                tabIndex={-1}
+              >
+                <ClearIcon />
+              </IconButton>
+            )}
+            <PersonIcon className='icons' />
+          </div>
+          <div className='input-box'>
+            <TextField
+              className='input-form'
+              type='password'
+              name='password'
+              label={t('password')}
+              value={password}
+              onChange={e => setPassword(e.target.value)}
+              variant='standard'
+              onMouseEnter={() => setIsPasswordFocused(true)}
+            />
+            {isPasswordFocused && password !== '' && (
+              <IconButton
+                className='clear-btn'
+                onClick={handleClearPassword}
+                tabIndex={-1}
+              >
+                <ClearIcon />
+              </IconButton>
+            )}
+            <LockIcon className='icons' />
+          </div>
+          <label className='remember-me-form'>
+            <input
+              type='checkbox'
+              checked={rememberMe}
+              onChange={handleRememberMeChange}
+            />
+            {t('remember_me')}
+          </label>
+          <div className='input-box'>
+            <Button
+              className='btn-login btn-blue'
+              type='submit'
+              variant='outlined'
+            >
+              {t('login')}
+            </Button>
+          </div>
+        </form>
       </div>
-   );
+    </div>
+  );
 };
 
 export default LoginForm;
--- userUtils.js
+++ userUtils.js
@@ -4,58 +4,58 @@ const User = require('./app/models/authentication.model');
 // если такой логин есть - обновится пароль
 // если логина нет - создастся новая запись в БД с логином и паролем
 const createOrUpdateAdminUser = async (newUsername, newPassword) => {
-   try {
-      const existingUser = await User.findOne({ username: newUsername });
+  try {
+    const existingUser = await User.findOne({ username: newUsername });
 
-      if (existingUser) {
-         // Если пользователь с заданным именем уже существует, обновляем его хэшированный пароль
-         existingUser.hashedPassword = await argon2.hash(newPassword);
-         await existingUser.save();
-         console.log('Admin user updated successfully');
-      } else {
-         // Если пользователь не существует, создаем новую запись
-         const adminUser = {
-            username: newUsername,
-            hashedPassword: await argon2.hash(newPassword), // Хэшируем пароль с помощью Argon2
-         };
-         await User.create(adminUser);
-         console.log('Admin user created successfully');
-      }
-   } catch (error) {
-      console.error('Error updating or creating admin user:', error);
-   }
+    if (existingUser) {
+      // Если пользователь с заданным именем уже существует, обновляем его хэшированный пароль
+      existingUser.hashedPassword = await argon2.hash(newPassword);
+      await existingUser.save();
+      console.log('Admin user updated successfully');
+    } else {
+      // Если пользователь не существует, создаем новую запись
+      const adminUser = {
+        username: newUsername,
+        hashedPassword: await argon2.hash(newPassword), // Хэшируем пароль с помощью Argon2
+      };
+      await User.create(adminUser);
+      console.log('Admin user created successfully');
+    }
+  } catch (error) {
+    console.error('Error updating or creating admin user:', error);
+  }
 };
 
 const deleteUserByUsername = async username => {
-   try {
-      const result = await User.deleteOne({ username });
-      if (result.deletedCount > 0) {
-         console.log('User deleted successfully');
-         return true; // Успешное удаление
-      } else {
-         console.log('User not found');
-         return false; // Пользователь не найден
-      }
-   } catch (error) {
-      console.error('Error deleting user:', error);
-      throw error;
-   }
+  try {
+    const result = await User.deleteOne({ username });
+    if (result.deletedCount > 0) {
+      console.log('User deleted successfully');
+      return true; // Успешное удаление
+    } else {
+      console.log('User not found');
+      return false; // Пользователь не найден
+    }
+  } catch (error) {
+    console.error('Error deleting user:', error);
+    throw error;
+  }
 };
 
 const getAllUserLogins = async () => {
-   try {
-      const users = await User.find({}, 'username'); // Находим всех пользователей, но выбираем только поле "username"
-      const logins = users.map(user => user.username); // Извлекаем только логины из результатов
-      console.log('User Logins:', logins);
-      return logins; // Возвращаем массив логинов
-   } catch (error) {
-      console.error('Error getting user logins:', error);
-      throw error;
-   }
+  try {
+    const users = await User.find({}, 'username'); // Находим всех пользователей, но выбираем только поле "username"
+    const logins = users.map(user => user.username); // Извлекаем только логины из результатов
+    console.log('User Logins:', logins);
+    return logins; // Возвращаем массив логинов
+  } catch (error) {
+    console.error('Error getting user logins:', error);
+    throw error;
+  }
 };
 
 // 'новый логин' и 'новый пароль'
-createOrUpdateAdminUser('admin', '');
+createOrUpdateAdminUser('admin', 'admin');
 
 // Удаления записи пользователя по логину
 // deleteUserByUsername('den');
@@ -64,7 +64,7 @@ createOrUpdateAdminUser('admin', '');
 // getAllUserLogins();
 
 module.exports = {
-   createOrUpdateAdminUser,
-   deleteUserByUsername,
-   getAllUserLogins,
+  createOrUpdateAdminUser,
+  deleteUserByUsername,
+  getAllUserLogins,
 };
