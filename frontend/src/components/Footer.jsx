88ccc5a8108669ed27382d9ef1b08120a884c754
Denis-Hish <denis.hish@gmail.com>
2026-01-09 11:47:23 +0100

    Change info in footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,9 +3,12 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer className='footer'>
-      {/* <div className="container"> */}
-      <p>Denis Nazarow &copy; 2026</p>
-      {/* </div> */}
+      <div className='container'>
+        <div className='content'>
+          <p>Denis Nazarow &copy; 2026</p>
+          <p>v.2.0</p>
+        </div>
+      </div>
     </footer>
   );
 };
